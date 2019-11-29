9b56a12b0070d2c683287838dd0d7f2ba12434b5
Lev Nazarov <leff@leff.su>
2019-11-29 11:18:56 +0300

    wip

--- app/src/main/java/org/nullpointerexception/sampler/ui/HomeScreen.kt
+++ app/src/main/java/org/nullpointerexception/sampler/ui/HomeScreen.kt
@@ -1,7 +1,15 @@
 package org.nullpointerexception.sampler.ui
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.nullpointerexception.sampler.R
 
 class HomeScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_home)
+    }
 
 }
